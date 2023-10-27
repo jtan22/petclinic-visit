dcd2aca59cf7427dcfec4a7d09e5b66444b93be4
Jason Tan <jtan20040401@gmail.com>
2023-10-27 20:45:04 +1100

    Enable spring security JDBC user manager

--- src/main/java/com/bw/petclinic/visit/security/VisitSecurityConfig.java
+++ src/main/java/com/bw/petclinic/visit/security/VisitSecurityConfig.java
@@ -9,21 +9,32 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class VisitSecurityConfig {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+//    @Bean
+    public UserDetailsManager inMemmoryUserDetailsManager() {
         UserDetails john = User.builder().username("john").password("{noop}john").roles("USER").build();
         UserDetails mary = User.builder().username("mary").password("{noop}mary").roles("USER", "ADMIN").build();
         return new InMemoryUserDetailsManager(john, mary);
     }
 
     @Bean
+    public UserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(customizer -> customizer
+                    .requestMatchers(HttpMethod.GET, "/actuator").authenticated()
+                    .requestMatchers(HttpMethod.GET, "/actuator/**").authenticated()
                     .requestMatchers(HttpMethod.GET, "/visits/pet/**").hasRole("USER")
                     .requestMatchers(HttpMethod.POST, "/visits/visit").hasRole("ADMIN"))
                 .httpBasic(Customizer.withDefaults())
